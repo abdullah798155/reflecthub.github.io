8eab8c1a9d42d6b75d57a49a19bce98d9828f3b4
reflecthub <reflect.intel@gmail.com>
2024-10-26 00:30:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,59 @@ window.onload = function() {
     
 };
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Wait for 5 seconds (5000 milliseconds)
+    // Wait for 1.3 seconds (1300 milliseconds)
     setTimeout(() => {
-        const swipersElement = document.querySelector('.swipers');
-        swipersElement.classList.add('start'); // Add the class to start the animation
+        // Select all elements with class names swipers and swipers1
+        const swipersElements = document.querySelectorAll('.swipers, .swipers1');
+
+        // Add the 'start' class to each selected element
+        swipersElements.forEach((element) => {
+            element.classList.add('start');
+        });
     }, 1300); // Delay in milliseconds
 });
+
+
+
+// Get elements
+const panelIcon = document.getElementById("panelIcon");
+const panelpopup = document.getElementById("popup");
+const panelclosePopup = document.getElementById("closePopup");
+
+// Open the popup when the panel icon is clicked
+panelIcon.onclick = function() {
+    panelpopup.style.display = "block";
+}
+
+// Close the popup when the close button is clicked
+panelclosePopup.onclick = function() {
+    panelpopup.style.display = "none";
+}
+
+// Close the popup when clicking outside of the popup content
+window.onclick = function(event) {
+    if (event.target == panelpopup) {
+        panelpopup.style.display = "none";
+    }
+}
+ const quickLink = document.getElementById("quickLink");
+ const quickpopup = document.getElementById("quickpopup");
+const quickclosePopup = document.getElementById("quickclosePopup");
+
+// Open the popup when the panel icon is clicked
+quickLink.onclick = function() {
+    quickpopup.style.display = "block";
+}
+
+// Close the popup when the close button is clicked
+quickclosePopup.onclick = function() {
+    quickpopup.style.display = "none";
+}
+
+// Close the popup when clicking outside of the popup content
+window.onclick = function(event) {
+    if (event.target == quickpopup) {
+        quickpopup.style.display = "none";
+    }
+}
+
