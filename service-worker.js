f4b07b4d58dc723269d6caa0abb47f6060c37f20
Mohammed Abdullah <reflect.intel@gmail.com>
2025-02-15 23:01:26 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "abdullah-pwa-cache-v9"; // Update this version to force a cache refresh
+const CACHE_NAME = "abdullah-pwa-cache-v10"; // Update this version to force a cache refresh
 
 const ASSETS_TO_CACHE = [
   "/Logos/Translate.png",
